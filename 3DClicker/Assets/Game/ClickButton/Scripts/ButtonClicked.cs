e3b951007867e1c774d65dbd53636cd4f52a0b9d
Nkn_nkc <nkc.souki.nakano@gmail.com>
2023-06-08 12:24:58 +0900

    Update:Readmeと操作にAnyKeyを追加

--- 3DClicker/Assets/Game/ClickButton/Scripts/ButtonClicked.cs
+++ 3DClicker/Assets/Game/ClickButton/Scripts/ButtonClicked.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using System;
+using DG.Tweening;
 
 public class ButtonClicked : MonoBehaviour, IClickable
 {
@@ -19,7 +20,7 @@ public class ButtonClicked : MonoBehaviour, IClickable
     public IObservable<int> ClickSubject => clickSubject;
 
     int clickPower = 1;
-
+    bool isAnimation = false;
     Rigidbody rb;
 
     private void Awake()
@@ -35,5 +36,36 @@ public class ButtonClicked : MonoBehaviour, IClickable
 
         Coin coin = Instantiate(this.coin, insPos.position, Quaternion.identity, coinParent);
         coin.RandomAddForce();
+
+        // アニメーションは重複しない
+        if (isAnimation) return;
+        //isAnimation = true;
+        //Vector3 beforePos = transform.position;
+        //transform.DOJump(beforePos, 0.1f, 1, 0.2f)
+        //    .OnComplete(() =>
+        //    {
+        //        isAnimation = false;
+        //        transform.position = beforePos;
+        //    });
+        //StartCoroutine(MoveAniamtion());
+    }
+
+    IEnumerator MoveAniamtion()
+    {
+        isAnimation = true;
+        Vector3 defaultPos = transform.position;
+        float destinationPosY = defaultPos.y + 0.1f;
+        for (float t = 0; t <= 1.0f; t += Time.deltaTime / 10)
+        {
+            float movePosY = Mathf.Lerp(destinationPosY, defaultPos.y, t);
+            transform.position = new Vector3(defaultPos.x, movePosY, defaultPos.z);
+        }
+        for (float t = 0; t <= 1.0f; t += Time.deltaTime / 10)
+        {
+            float movePosY = Mathf.Lerp(defaultPos.y, destinationPosY, t);
+            transform.position = new Vector3(defaultPos.x, movePosY, defaultPos.z);
+        }
+        isAnimation = false;
+        yield return null;
     }
 }
